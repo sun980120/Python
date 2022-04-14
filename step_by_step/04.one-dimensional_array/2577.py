3d8990b79f7ee3a3bf8ced94a7e7d0ce17b0723b
우성 홍 <20171302@vision.hoseo.edu>
2022-04-14 13:47:15 +0900

    Edit : py

--- step_by_step/04.one-dimensional_array/2577.py
+++ step_by_step/04.one-dimensional_array/2577.py
@@ -1,14 +1,22 @@
-# 숫자의 개수
+# # 숫자의 개수
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# sum = a * b * c
+# number_list=[0,0,0,0,0,0,0,0,0,0]
+# while sum!=0:
+#     data = sum % 10
+#     sum = sum // 10
+#     for i in range(10):
+#         if i==data:
+#             number_list[i]+=1
+# for i in range(10):
+#     print(number_list[i])
+
 a = int(input())
 b = int(input())
 c = int(input())
-sum = a * b * c
-number_list=[0,0,0,0,0,0,0,0,0,0]
-while sum!=0:
-    data = sum % 10
-    sum = sum // 10
-    for i in range(10):
-        if i==data:
-            number_list[i]+=1
+
+result = list(str(a * b * c))
 for i in range(10):
-    print(number_list[i])
+    print(result.count(str(i)))
